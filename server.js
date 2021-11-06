4db5c3bdd6024cea58d5be9e87ba91f438c15c20
resobyte <iresatakcan.bm@gmail.com>
2021-11-07 01:34:46 +0300

    [refactor]: Moved all folder app folder

--- server.js
+++ server.js
@@ -17,6 +17,9 @@ app.get('/', (req, res) => {
     res.json({ message: "Welcome resobyte boilerPlate application." });
 });
 
+require('./app/routes/auth.routes')(app);
+require('./app/routes/user.routes')(app);
+
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}.`);
